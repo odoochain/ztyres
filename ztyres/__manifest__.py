e096ac1909f972e52958220b2a5b963a74f9dd13
José Roberto Mejía Pacheco <isscjrmpacheco@gmail.com>
2022-11-28 03:18:43 -0600

    [UPD] Test

--- ztyres/__manifest__.py
+++ ztyres/__manifest__.py
@@ -19,7 +19,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Customizations',
-    'version': '14.0.0.0',
+    'version': '14.0.0.1',
     'external_dependencies': {
         'python': [
             
