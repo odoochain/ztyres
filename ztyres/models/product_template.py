53bfbab52c7c8382de8dd34a42785d38d22239b9
José Roberto Mejía Pacheco <isscjrmpacheco@gmail.com>
2022-12-05 09:38:34 -0600

    [UPD] Revome spaces to update name.

--- ztyres/models/product_template.py
+++ ztyres/models/product_template.py
@@ -9,7 +9,7 @@ class ProductTemplate(models.Model):
 
     def update_name_ztyres(self):
         for record in self:            
-            name = "%s %s %s %s %s %s"%(self.x_studio_medida_1 or "",self.x_studio_cara or "",self.x_studio_capas or "",self.x_studio_ndice_de_velocidad or "",self.x_studio_marca or "",self.x_studio_modelo_1 or "")
+            name = "%s%s%s%s%s%s"%(self.x_studio_medida_1 or "",self.x_studio_cara or "",self.x_studio_capas or "",self.x_studio_ndice_de_velocidad or "",self.x_studio_marca or "",self.x_studio_modelo_1 or "")
             record.name = name
             record.display_name = name
         
