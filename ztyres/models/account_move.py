2e92375cb9cbcb60acfee6ea90f82d10f05dada6
José Roberto Mejía Pacheco <isscjrmpacheco@gmail.com>
2023-02-01 15:26:34 -0600

    [UPD] Payment Terms

--- ztyres/models/account_move.py
+++ ztyres/models/account_move.py
@@ -21,21 +21,13 @@ class AccountMove(models.Model):
 
     @api.depends('move_type', 'invoice_date_due', 'invoice_date', 'invoice_payment_term_id', 'invoice_payment_term_id.line_ids')
     def _compute_l10n_mx_edi_payment_policy(self):     
-        for move in self:
-            if move.is_invoice(include_receipts=True) and move.invoice_date_due and move.invoice_date:
-                if move.move_type == 'out_invoice':
-                    # In CFDI 3.3 - rule 2.7.1.43 which establish that
-                    # invoice payment term should be PPD as soon as the due date
-                    # is after the last day of  the month (the month of the invoice date).
-                    if move.invoice_date_due.month > move.invoice_date.month or \
-                       move.invoice_date_due.year > move.invoice_date.year or \
-                       len(move.invoice_payment_term_id.line_ids) > 1:  # to be able to force PPD
-                        move.l10n_mx_edi_payment_policy = 'PPD'
-                    else:
-                        move.l10n_mx_edi_payment_policy = 'PPD'                
-                else:
+        for move in self:             
+            if move.move_type == 'out_invoice':
+                if move.invoice_payment_term_id.line_ids.mapped('days') > 0:
                     move.l10n_mx_edi_payment_policy = 'PPD'
+                else:
+                    move.l10n_mx_edi_payment_policy = 'PUE'
             elif move.move_type == 'out_refund':
                 move.l10n_mx_edi_payment_policy = 'PUE'
             else:
-                move.l10n_mx_edi_payment_policy = 'PPD'
+                move.l10n_mx_edi_payment_policy = False
